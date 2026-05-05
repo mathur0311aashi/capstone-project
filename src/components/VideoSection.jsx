bad0825d10cd3d6ad06815f101550890f88a5aa3
Aashi Mathur <mathuraashi0311@gmail.com>
2026-05-06 01:52:19 +0530

    complete project

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getDestinationKeyword } from '../utils/helpers'; // This is correct now!
+import { getDestinationKeyword } from '../utils/Helpers'; // This is correct now!
 
 const VideoSection = ({ destination }) => {
   // ... rest of your component code
